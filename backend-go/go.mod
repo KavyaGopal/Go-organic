bc691d944b8bf565bd9b8bbfee98ae1b05d0ae8f
Ashwin Rai <raiashwin018@gmail.com>
2022-02-03 14:16:23 -0500

    Update: Setup Mock APIs

--- backend-go/go.mod
+++ backend-go/go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/gin-gonic/gin v1.7.7
+	github.com/gorilla/mux v1.8.0
 	gorm.io/driver/sqlite v1.2.6
 	gorm.io/gorm v1.22.5
 )
---
+++ backend-go/pkg/api/api.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/KavyaGopal/Go-organic/backend-go/pkg/model"
+	"encoding/json"
+)
+	
+//init product variable for mock
+var products []model.Product
+
+//get all products
+func getProducts(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+
+}
+
+//get single book
+func getProduct(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get the params
+	// loop through products and find the id
+	for _, item := range products {
+		if item .ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return 
+		}
+	}
+	json.NewEncoder(w).Encode(&model.Product{})
+}
+
+
+func main() {
+	//init router
+	r := mux.NewRouter()
+
+	//Mock data TODO: get the data from the sqlite
+	products = append(products, model.Product{ID: "1", Name: "Groceries"})
+	products = append(products, model.Product{ID: "2", Name: "Fruits"})
+	products = append(products, model.Product{ID: "3", Name: "Snacks"})
+	products = append(products, model.Product{ID: "4", Name: "Vegetables"})
+	products = append(products, model.Product{ID: "5", Name: "Dairy"})
+
+	r.HandleFunc("/api/products", getProducts).Methods("GET")
+	r.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000",r))
+
+}

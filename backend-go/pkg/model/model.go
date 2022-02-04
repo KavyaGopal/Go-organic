ebc14eb6dcda50c17e95c6acff5d5d23909be2f5
zubin-arya <virtuoso02031995@gmail.com>
2022-02-04 18:10:10 -0500

    DB Model update

--- backend-go/pkg/model/model.go
+++ backend-go/pkg/model/model.go
@@ -1,7 +1,18 @@
 package model
 
+import "gorm.io/gorm"
+
 // Product Struct (Model)
-type ProductMock struct{
-	ID   string `json:"id"`
-	Name string `json:"name"`
+type ProdMaster struct {
+	gorm.Model
+	ID          int    `gorm:"primary_key" json:"id"`
+	Name        string `gorm:"type:varchar(255);NOT NULL" json:"name"`
+	Category    CategoryMaster
+	Price       float32 `gorm:"type:decimal(10,2)" json:"price"`
+	Description string  `gorm:"type:text" json:"description"`
+}
+type CategoryMaster struct {
+	gorm.Model
+	CatID        int    `gorm:"primary_key" json:"catid"`
+	CategoryName string `gorm:"type:varchar(255)" json:"categoryname"`
 }

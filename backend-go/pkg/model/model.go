a74963cca4ac38f05322259605d07a312fd86306
Ashwin Rai <raiashwin018@gmail.com>
2022-02-26 19:34:54 -0500

    Update: added backend changes for fruits

--- backend-go/pkg/model/model.go
+++ backend-go/pkg/model/model.go
@@ -43,3 +43,13 @@ type CategoryMock struct {
 	Price       string `json:"price"`
 	Rating      string `json:"rating"`
 }
+
+type FruitMock struct {
+	ID            int64   `json:"id"`
+	ImageSource   string   `json:"imgSrc"`
+	ItemName      string `json:"itemName"`
+	ItemDesc      string   `json:"itemDesc"`
+	ItemWeight 	  int64   `json:"itemWt"`
+	ItemQuantity  int64 `json:"itemQuantity"`
+	ItemCost      int64 `json:"itemCost"`
+}

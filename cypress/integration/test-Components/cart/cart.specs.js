a97a766da3f87d35a1a4308a48f867d76f7309a0
Vaibhav Kulkarni <kulvaibhavp@gmail.com>
2022-04-01 18:12:57 -0400

    Added tests for checkout component and for routing connection

--- cypress/integration/test-Components/cart/cart.specs.js
+++ cypress/integration/test-Components/cart/cart.specs.js
@@ -2,22 +2,31 @@
  import Chance from 'chance';
  const chance = new Chance();
 
- describe('Tests Cart Component', ()=> {
+ describe('Tests Cart Component', () => {
      // check if the cart component is loading
      it('Visits the Go organic Cart Component', () => {
-       cy.visit('http://localhost:4200/cart')
+         cy.visit('http://localhost:4200/cart')
      })
 
      // check if the cart component has following elements
      it('Checks if the cart has following elements', () => {
-      cy.visit('http://localhost:4200/cart')
-      cy.contains('ID')
-      cy.contains('Name')
-      cy.contains('Item')
-      cy.contains('Cost')
-      cy.contains('Quantity')
-      cy.contains('Sub Total')
-      cy.contains('Actions')
-      cy.contains('Checkout')
-    });
+         cy.visit('http://localhost:4200/cart')
+         cy.contains('ID')
+         cy.contains('Name')
+         cy.contains('Item')
+         cy.contains('Cost')
+         cy.contains('Quantity')
+         cy.contains('Sub Total')
+         cy.contains('Actions')
+         cy.contains('Proceed to Payment')
+     });
+
+     // checks if button is clickable and it's routing to the next component
+     it('Visits the Go organic Cart Component', () => {
+         cy.visit('http://localhost:4200/cart')
+         cy.get('.center > .mat-focus-indicator > .mat-button-wrapper').click();
+         cy.url().should('include', '/checkout')
+     })
+
+
  });
